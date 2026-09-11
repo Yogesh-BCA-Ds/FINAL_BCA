416a7aee691408ff3370afc158562495803a66cf
yogesh-bca-ds <yogeshwanocoder@gmail.com>
2026-09-11 22:37:12 +0530

    basic calculator structure

---
+++ script.js
@@ -0,0 +1,22 @@
+function getValue(){
+    let a1 = document.getElementById("1").value;
+    let a2 = document.getElementById("2").value;
+    let a3 = document.getElementById("3").value;
+    let a4 = document.getElementById("4").value;
+    let a5 = document.getElementById("5").value;
+    let a6 = document.getElementById("6").value;
+    let a7 = document.getElementById("7").value;
+    let a8 = document.getElementById("8").value;
+    let a9 = document.getElementById("9").value;
+    let a0 = document.getElementById("0").value;
+    document.getElementById("i").value=a1;
+    document.getElementById("i").value=a2;
+    document.getElementById("i").value=a3;
+    document.getElementById("i").value=a4;
+    document.getElementById("i").value=a5;
+    document.getElementById("i").value=a6;
+    document.getElementById("i").value=a7;
+    document.getElementById("i").value=a8;
+    document.getElementById("i").value=a9;
+    document.getElementById("i").value=a0;
+    }
