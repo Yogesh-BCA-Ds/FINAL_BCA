d454ca1f7d025a2c97d4675d3cc4429e2d5b4239
yogesh-bca-ds <yogeshwanocoder@gmail.com>
2026-09-12 00:17:39 +0530

    js function

--- frontend/calculator/script.js
+++ frontend/calculator/script.js
@@ -1,22 +1,7 @@
+let num1;
+let num2;
 function getValue(){
-    let a1 = document.getElementById("1").value;
-    let a2 = document.getElementById("2").value;
-    let a3 = document.getElementById("3").value;
-    let a4 = document.getElementById("4").value;
-    let a5 = document.getElementById("5").value;
-    let a6 = document.getElementById("6").value;
-    let a7 = document.getElementById("7").value;
-    let a8 = document.getElementById("8").value;
-    let a9 = document.getElementById("9").value;
-    let a0 = document.getElementById("0").value;
-    document.getElementById("i").value=a1;
-    document.getElementById("i").value=a2;
-    document.getElementById("i").value=a3;
-    document.getElementById("i").value=a4;
-    document.getElementById("i").value=a5;
-    document.getElementById("i").value=a6;
-    document.getElementById("i").value=a7;
-    document.getElementById("i").value=a8;
-    document.getElementById("i").value=a9;
-    document.getElementById("i").value=a0;
+    let a = document.getElementById("i").value=document.getElementById("i").value+event.target.value;
+    
+    
     }
