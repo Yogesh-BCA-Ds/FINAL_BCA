7587edfc659bf1e87450dcc1f98a392c091e3995
yogesh-bca-ds <yogeshwanocoder@gmail.com>
2026-09-12 17:21:23 +0530

    experimenting with calculation method

--- frontend/calculator/script.js
+++ frontend/calculator/script.js
@@ -1,10 +1,26 @@
-let num1;
-let num2;
 function getValue(){
-    let a = document.getElementById("i").value=document.getElementById("i").value+event.target.value;
+    let num1;
+    let a;
+    let b;
+    let sum;
+    if ((event.target.value) != "+"){
+        a = document.getElementById("i").value=document.getElementById("i").value+event.target.value;
+        num1 = Number(a);
+        console.log(num1);
+        }
+    else{
+        document.getElementById("i").value = "";
+        if ((event.target.value) != "="){
+            b = document.getElementById("i").value=document.getElementById("i").value+event.target.value; 
+            let num2 = Number(b);
+            sum = num1+num2;
+            console.log(sum);
+        }
+        else{
+        document.getElementById("i").value = "";
+        }
+        }
 
-    if (event.target.value){
-        num1 = a;
-    }
+  
 }
-console.log(num1);
+/*console.log(num1);*/
