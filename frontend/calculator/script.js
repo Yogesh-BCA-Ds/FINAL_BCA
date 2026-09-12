c70241ebe77336400cec1a2368529f647c0a2b06
yogesh-bca-ds <yogeshwanocoder@gmail.com>
2026-09-12 16:23:56 +0530

    experimenting with getting values

--- frontend/calculator/script.js
+++ frontend/calculator/script.js
@@ -2,6 +2,9 @@ let num1;
 let num2;
 function getValue(){
     let a = document.getElementById("i").value=document.getElementById("i").value+event.target.value;
-    
-    
+
+    if (event.target.value){
+        num1 = a;
     }
+}
+console.log(num1);
