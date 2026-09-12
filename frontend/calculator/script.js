829e4ca2b264e8d39679b779147ed05357ca4087
yogesh-bca-ds <yogeshwanocoder@gmail.com>
2026-09-12 20:41:05 +0530

    basic math operation added

--- frontend/calculator/script.js
+++ frontend/calculator/script.js
@@ -1,26 +1,50 @@
+let num1;
+let num2;
+let flag = false;
 function getValue(){
-    let num1;
     let a;
     let b;
+    let op;
     let sum;
-    if ((event.target.value) != "+"){
+    if (event.target.value != "+"|"-"|"/"|"*"|"%" && flag==false){
         a = document.getElementById("i").value=document.getElementById("i").value+event.target.value;
         num1 = Number(a);
-        console.log(num1);
         }
-    else{
-        document.getElementById("i").value = "";
-        if ((event.target.value) != "="){
-            b = document.getElementById("i").value=document.getElementById("i").value+event.target.value; 
-            let num2 = Number(b);
-            sum = num1+num2;
-            console.log(sum);
-        }
-        else{
+    else if (event.target.value == "+"|"-"|"/"|"*"|"%"){
+        flag = true;
+        op = event.target.value;
         document.getElementById("i").value = "";
         }
+    else if (event.target.value != "=" && flag == true)
+    {
+        b = document.getElementById("i").value = document.getElementById("i").value+event.target.value;
+        num2 = Number(b);
+        
+    }
+    else if (event.target.value == "=" && op=="+"){
+        sum = num1+num2;
+        document.getElementById("i").value = sum;
         }
 
+   else if (event.target.value == "=" && op=="-"){
+        let dif = num1-num2;
+        document.getElementById("i").value = dif;
+        }
+
+    else if (event.target.value == "=" && op=="*"){
+        let prod = num1*num2;
+        document.getElementById("i").value = prod;
+        }
+
+    else if (event.target.value == "=" && op=="/"){
+        let div  = num1/num2;
+        document.getElementById("i").value = div;
+        }
+    else if (event.target.value == "clear"){
+        document.getElementById("i").value = "";
+        num1 = 0;
+        num2 = 0;
+        }
   
 }
 /*console.log(num1);*/
